defb74802a7ee1342dffc7fe727ee32df6192620
Elainedc <156939191+Elainedc@users.noreply.github.com>
2024-02-18 21:30:42 -0300

    Rodapé

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ function criptografar() {
                                     .replace(/u/g, "ufat");
     document.getElementById('output').innerHTML = resultadoCriptografia;
 }
-/*
+
 function descriptografar() {
-    let texto = textInput.value;
-
-    let resultadoDescriptografia = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-    document.getElementById('output').innertext = '<textarea readonly id= "input__texto">' + resultadoDescriptografia + '</textarea>' + '<button class="botao__descriptografar" id="output" onclick="descriptografar()">Descriptografar</button>';
+    let textInput = document.querySelector("#input__texto").value;
+    let resultadoCriptografia = textInput.replace(/enter/g, "e")
+                                    .replace(/imes/g, "i")
+                                    .replace(/ai/g, "a")
+                                    .replace(/ober/g, "o")
+                                    .replace(/ufat/g, "u");
+    document.getElementById('output').innerHTML = resultadoCriptografia;
 }
 
 function copiar() {
@@ -25,4 +27,4 @@ function copiar() {
     document.execCommand('copy');
     alert('O texto foi copiado!');
 }
-*/
+
