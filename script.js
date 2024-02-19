a8ae50bf5d73cacac8f568b66d23c7f59d876651
Elainedc <156939191+Elainedc@users.noreply.github.com>
2024-02-19 14:00:34 -0300

    Botão copiar

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ function criptografar() {
                                     .replace(/a/g, "ai")
                                     .replace(/o/g, "ober")
                                     .replace(/u/g, "ufat");
-    document.getElementById('output').innerHTML = resultadoCriptografia;
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="input__texto">' + resultadoCriptografia + '</textarea>' + '<button class="botao__copiar" onclick="copiar()">Copiar</button>'
 }
 
 function descriptografar() {
     let textInput = document.querySelector("#input__texto").value;
-    let resultadoCriptografia = textInput.replace(/enter/g, "e")
+    let resultadoDescriptografia = textInput.replace(/enter/g, "e")
                                     .replace(/imes/g, "i")
                                     .replace(/ai/g, "a")
                                     .replace(/ober/g, "o")
                                     .replace(/ufat/g, "u");
-    document.getElementById('output').innerHTML = resultadoCriptografia;
+    document.getElementById('output').innerHTML = '<textarea readonly id="input__texto">' +  resultadoDescriptografia+ '</textarea>' + '<button class="botao__copiar" onclick="copiar()">Copiar</button>';
 }
 
 function copiar() {
